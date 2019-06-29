6bce0ff2205342e8e361bb6157bca140d6b48747
Zach Johnson <zm9171@gmail.com>
2019-06-29 09:42:33 -0500

    difference between company and influencer routes

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,9 @@
 let hashPass = require('hashPass');
 let uuidv1 = require('uuid/v1');
-let users = require("../models/user");
+let users = require('../models/user')
 
 let user = {
-    create: function(request, response){
+    createCompany: function(request, response){
         if (!request.body.email.includes('@') || !request.body.email.includes('.')){
             response.status(400).json({'error': 'email is not valid'});
         } else if (request.body.password !== request.body.password_confirm){
@@ -15,7 +15,7 @@ let user = {
                 password: hashedPassword.hash,
                 salt: hashedPassword.salt
             };
-            users.insertNew(userRequest, function(error, result){
+            users.insertNewCompany(userRequest, function(error, result){
                 if (error){
                     console.log(error);
                     if (error.sqlMessage.includes('Duplicate')){
@@ -31,9 +31,40 @@ let user = {
                 }
             });
         }
+        console.log("company was created");
     },
-    login: function(request, response){
-        users.selectByEmail(request.body.email, function(error, result){
+    createInfluencer: function(request, response){
+        if (!request.body.email.includes('@') || !request.body.email.includes('.')){
+            response.status(400).json({'error': 'email is not valid'});
+        } else if (request.body.password !== request.body.password_confirm){
+            response.status(400).json({'error': 'passwords do not match'});
+        } else {
+            let hashedPassword = hashPass(request.body.password);
+            let userRequest = {
+                email: request.body.email,
+                password: hashedPassword.hash,
+                salt: hashedPassword.salt
+            };
+            users.insertNewInfluencer(userRequest, function(error, result){
+                if (error){
+                    console.log(error);
+                    if (error.sqlMessage.includes('Duplicate')){
+                        response.status(400).json({'error': 'email already exists in system'});
+                    }else{
+                        response.status(500).json({'error': 'oops we did something bad'});
+                    }
+                }else{
+                    response.json({
+                        user_id: result.insertId,
+                        email: userRequest.email
+                    });
+                }
+            });
+        }
+        console.log("influencer was created")
+    },
+    loginCompany: function(request, response){
+        users.selectByCompanyEmail(request.body.email, function(error, result){
             if (error){
                 console.log(error);
                 response.status(500).json({'error': 'oops we did something bad'});
@@ -55,26 +86,66 @@ let user = {
                 }
             }
         });
+        console.log("company was logged in");
+    },
+    loginInfluencer: function(request, response){
+        users.selectByInfluencerEmail(request.body.email, function(error, result){
+            if (error){
+                console.log(error);
+                response.status(500).json({'error': 'oops we did something bad'});
+            } else if(!result.length) {
+                response.status(404).json({'error': 'user not found'});
+            } else {
+                user = result[0];
+                loginAttempt = hashPass(request.body.password, user.salt);
+                if (loginAttempt.hash === user.password){
+                    let uuid = uuidv1();
+                    users.updateSession(user.email, uuid, function(error, result) {
+                        delete user.password;
+                        delete user.salt;
+                        delete user.session;
+                        response.header('x-session-token', uuid).json(user);
+                    });
+                }else{
+                    response.status(401).json({'error': 'improper login credentials'});
+                }
+            }
+        });
+        console.log("influencer logged in");
     },
     logout: function(request, response){
         users.removeSession(request.headers['x-session-token'], function(error, result){
             response.json({'message': 'user logged out successfully'});
         });
     },
-    getMyself: function(request, response){
-        users.getMyself(request.headers['x-session-token'], function(error, result){
+    getCompany: function(request, response){
+        users.getCompany(request.headers['x-session-token'], function(error, result){
             response.json(result[0]);
         });
     },
-    getUserByID: function(request, response){
-        users.getUserByID(request.params.id, function(error, result){
+    getInfluencer: function(request, response){
+        users.getInfluencer(request.headers['x-session-token'], function(error, result){
+            response.json(result[0]);
+        });
+    },
+    getCompanyID: function(request, response){
+        users.getCompanyID(request.params.id, function(error, result){
             if (result.length){
                 response.json(result[0]);
             } else {
                 response.status(404).json({'error': 'user not found'});
             }
         });
-    }
+    },
+    getInfluencerID: function(request, response){
+        users.getInfluencerID(request.params.id, function(error, result){
+            if (result.length){
+                response.json(result[0]);
+            } else {
+                response.status(404).json({'error': 'user not found'});
+            }
+        });
+    },
 };
 
 module.exports = user;
--- routes.js
+++ routes.js
@@ -2,22 +2,28 @@ var user = require("./controllers/user");
 var authorizer = require("./controllers/config/authorizer");
 
 module.exports = function(app) {
-    app.post("/api/user", function(request, respsonse) {
-        user.create(request, respsonse);
+    app.post("/api/company", function(request, response) {
+        user.createCompany(request, response);
     });
-    app.post("/api/user/login", function(request, respsonse) {
+    app.post("/api/influencer", function(request, response) {
+        user.createInfluencer(request, response);
+    });
+    app.post("/api/company/login", function(request, response) {
         user.login(request, response);
     });
-    app.delete("/api/user/login", function(request, response) {
+    app.post("api/influencer/login", function(request,response){
+        user.login(request, response);
+    })
+    app.delete("/api/company/login", function(request, response) {
         user.logout(request, response);
     });
-    app.get("/api/user", authorizer.authenticate, function(request, response) {
+    app.get("/api/company", authorizer.authenticate, function(request, response) {
         user.getCompany(request, response);
     });
-    app.get("/api/user", authorizer.authenticate, function(request, response) {
+    app.get("/api/influencer", authorizer.authenticate, function(request, response) {
         user.getInfluencer(request, response);
     });
-    app.get("/api/user/:id", authorizer.authenticate, function(request, response) {
-        user.getUserByID(request, response);
+    app.get("/api/company/:id", authorizer.authenticate, function(request, response) {
+        user.getCompanyByID(request, response);
     });
 };
