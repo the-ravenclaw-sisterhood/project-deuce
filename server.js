d8f4c1df2aff60c4daf4d339999d803f5b592563
bee25141 <bee25141@gmail.com>
2019-06-29 11:23:25 -0500

    working on signUp user

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 var port = process.env.PORT || 9000;
 var config = require('./config');
 
-require('./routes');
+require('./routes')(app);
 
 app.use(express.static(__dirname + '/views')); // you should change this to be wherever your html files are
 app.use(express.urlencoded({extended: true}));
